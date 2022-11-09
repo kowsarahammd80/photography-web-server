490c4dda693441e31a8606c0b6615a2b74562e53
kowsarahammed80 <kowsarahammed80@gmail.com>
2022-11-09 19:38:28 +0600

    api-get-done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
-require('mongodb');
+
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,7 +23,24 @@ async function run() {
   
   try{
 
-    
+    const serviceCollection = client.db('photography').collection('services');
+
+     app.get('/services', async (req, res) => {
+         
+      const query = {}
+      const curser = serviceCollection.find(query);
+      const services = await curser.toArray();
+      res.send(services)
+
+     })
+
+     app.get('/services/:id', async(req, res) => {
+       
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const service = await serviceCollection.findOne(query);
+      res.send(service)
+     })
 
   }
   finally{
@@ -31,6 +48,7 @@ async function run() {
   }
 
 }
+run().catch(err => console.error(err))
 
 
 
